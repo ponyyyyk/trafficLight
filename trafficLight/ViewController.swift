fd499d0974586d31e828e74deeabaf8441494f70
v.v.shumakov <shumakwas@gmail.com>
2023-05-18 06:04:23 +0500

    Work with logic

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var switchLightButton: UIButton!
+    private let lightIsOff = 0.3
+    private let lightIsOn = 1.0
+    private var currentColor = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        switchLightButton.layer.cornerRadius = 20
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
+    
+    @IBAction func switchLightButtonDidTapped() {
+        switchLightButton.setTitle("NEXT", for: .normal)
+        
+        switch currentColor {
+        case "Red":
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentColor = "Yellow"
+        case "Yellow":
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            currentColor = "Green"
+        default:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentColor = "Red"
+        }
+    }
+    
 }
 
