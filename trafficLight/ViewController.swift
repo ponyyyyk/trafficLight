9559a4ae2c4ff333132d046268645d61dfeec878
v.v.shumakov <shumakwas@gmail.com>
2023-05-25 18:07:58 +0500

    some code fixes

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -12,44 +12,57 @@ class ViewController: UIViewController {
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
+    
     @IBOutlet var switchLightButton: UIButton!
+    
     private let lightIsOff = 0.3
     private let lightIsOn = 1.0
-    private var currentColor = ""
+    private var currentColor = CurrentColor.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         redLight.alpha = lightIsOff
         yellowLight.alpha = lightIsOff
         greenLight.alpha = lightIsOff
+        
         switchLightButton.layer.cornerRadius = 20
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
+        
         redLight.layer.cornerRadius = redLight.frame.width / 2
         yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
         greenLight.layer.cornerRadius = greenLight.frame.width / 2
     }
     
     @IBAction func switchLightButtonDidTapped() {
-        switchLightButton.setTitle("NEXT", for: .normal)
+        if switchLightButton.currentTitle == "START" {
+            switchLightButton.setTitle("NEXT", for: .normal)
+        }
         
         switch currentColor {
-        case "Red":
-            redLight.alpha = lightIsOff
-            yellowLight.alpha = lightIsOn
-            currentColor = "Yellow"
-        case "Yellow":
-            yellowLight.alpha = lightIsOff
-            greenLight.alpha = lightIsOn
-            currentColor = "Green"
-        default:
+        case .red:
             greenLight.alpha = lightIsOff
             redLight.alpha = lightIsOn
-            currentColor = "Red"
+            currentColor = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentColor = .green
+        case .green:
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            currentColor = .red
         }
     }
     
+}
+
+extension ViewController {
+    private enum CurrentColor {
+        case red, yellow, green
+    }
 }
 
